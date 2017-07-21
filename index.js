5d54a78e38f14c463e534d93b5df0a76c2f8bc67
Andrey Lalev <andypyrope@gmail.com>
2017-07-21 04:39:34 +0300

    Test + version bump(patch)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+"use strict";
+
 console.log('foo');
-var Parser = require('./dist/main/Parser').Parser;
-var path = require('path');
-new Parser(path.resolve(__dirname, 'project.xml'));
+module.exports = require('./dist/main/Parser').Parser;

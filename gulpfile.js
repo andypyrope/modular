18eeb16b18b27d15bbbd3a2b05dae054d0bd0d7a
Andrey Lalev <andypyrope@gmail.com>
2017-07-26 05:57:57 +0300

    Fix dist directory clean-up
    Functionality:
     - Make the build task emit to a temporary build directory (dist-tmp) before deleting the original build directory and replacing it
    
    Code quality:
     - Turn some variables into constants
     - Fix some whitespace
    
    [Issue](https://github.com/andypyrope/modular/issues/10)
    [Project card](https://github.com/andypyrope/modular/projects/1#card-3918407)

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,39 @@ var tslint = require('gulp-tslint');
 var cleanDest = require('gulp-clean-dest');
 var gulpIgnore = require('gulp-ignore');
 var MyReporter = require('./MyReporter');
+const del = require('del');
+const fs = require('fs');
 
 // ========
 // ======== BUILD
 // ========
 
-var buildDir = path.join('.', 'dist');
-var srcDir = path.join('.', 'src');
+const CWD = process.cwd();
+const BUILD_DIR = path.join(CWD, 'dist');
+const TMP_BUILD_DIR = path.join(CWD, 'dist-tmp');
+const SRC_DIR = path.join(CWD, 'src');
+
 gulp.task('lint', function () {
-   return gulp.src(path.join(srcDir, '**', '*.ts'))
-      .pipe(gulpIgnore.exclude(path.join(srcDir, '**', '*.d.ts')))
+   return gulp.src(path.join(SRC_DIR, '**', '*.ts'))
+      .pipe(gulpIgnore.exclude(path.join(SRC_DIR, '**', '*.d.ts')))
       .pipe(tslint({
          formatter: 'verbose'
       }))
       .pipe(tslint.report());
 });
+
+gulp.task('clean', function (done) {
+   del([BUILD_DIR]).then(function (test) {
+      done();
+   }).catch(function (err) {
+      console.error(err.message);
+   });
+});
+
+gulp.task('rename', function () {
+   fs.renameSync(TMP_BUILD_DIR, BUILD_DIR);
+});
+
 gulp.task('gen', function () {
    var tsProject = typescript.createProject(path.join('.', 'tsconfig.json'), {
       typescript: require('typescript')
@@ -31,11 +49,11 @@ gulp.task('gen', function () {
       .pipe(sourcemaps.init())
       .pipe(tsProject()).js
       .pipe(sourcemaps.write('.', { sourceRoot: '.' }))
-      .pipe(cleanDest(buildDir))
-      .pipe(gulp.dest(buildDir));
+      .pipe(cleanDest(TMP_BUILD_DIR))
+      .pipe(gulp.dest(TMP_BUILD_DIR));
 });
 gulp.task('build', function (callback) {
-   runSequence('lint', 'gen', callback);
+   runSequence('lint', 'gen', 'clean', 'rename', callback);
 });
 
 // ========
@@ -44,7 +62,7 @@ gulp.task('build', function (callback) {
 
 gulp.task('test-only', function () {
    return gulp.src('./dist/spec/**/*.js')
-      .pipe(jasmine({ reporter: new MyReporter(srcDir) }));
+      .pipe(jasmine({ reporter: new MyReporter(SRC_DIR) }));
 });
 gulp.task('test', function (callback) {
    runSequence('build', 'test-only', callback);
